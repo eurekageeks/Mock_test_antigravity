a68eb063287a1183c4b16be25540490f6c26d444
eurekageeks <54979906+eurekageeks@users.noreply.github.com>
2026-07-21 16:36:06 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://52.34.197.198:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://200.141.0.169:8000',
   headers: {
     'Content-Type': 'application/json',
   },
